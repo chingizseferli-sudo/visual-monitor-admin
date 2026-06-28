6d6ee960876e3060581d8dbe98b0e2fdc052fb8d
SysAdmin <chingizseferli@gmail.com>
2026-06-28 16:43:38 +0400

    polish: update visual monitor branding

--- src/components/layout/app-title.tsx
+++ src/components/layout/app-title.tsx
@@ -25,8 +25,8 @@ export function AppTitle() {
               onClick={() => setOpenMobile(false)}
               className='grid flex-1 text-start text-sm leading-tight'
             >
-              <span className='truncate font-bold'>Shadcn-Admin</span>
-              <span className='truncate text-xs'>Vite + ShadcnUI</span>
+              <span className='truncate font-bold'>Visual Monitor</span>
+              <span className='truncate text-xs'>Media Monitorinq</span>
             </Link>
             <ToggleSidebar />
           </div>
